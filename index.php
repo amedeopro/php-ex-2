495aabf2073259d7e895bd924836aa563773ca49
amedeopro <amedeopro@me.com>
2019-01-22 22:23:47 +0100

    esercizio 1 completato

--- index.php
+++ index.php
@@ -2,9 +2,29 @@
 <html lang="it" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>Password</title>
   </head>
   <body>
+
+    <?php $password = $_GET['password']; ?>
+
+      <?php if ($password === 'boolean') { ?>
+        <h2 style='color: green;'>Password Corretta</h2>
+      <?php } else if ($password === NULL) { ?>
+
+        <h2 style='color: yellow;'>Devi inserire la password</h2>
+
+      <?php } else if ($password !== 'boolean')  { ?>
+
+        <h2 style='color: red;'>Password errata</h2>
+
+      <?php }; ?>
+
+
+
+
+
+
 
   </body>
 </html>
--- index2.php
+++ index2.php
@@ -1,3 +1,12 @@
+<!-- Creiamo un array contenente le partite di basket di un'ipotetica tappa del
+calendario. Ogni array avrà una squadra di casa e una squadra ospite.
+Avremo poi sempre per ogni array i punti fatti dalla squadra di casa e i
+punti fatti dalla squadra in trasferta. Stampiamo a schermo tutte le
+partite con questo schema, tenendo conto che il punteggio potrebbe non
+essere disponibile
+Olimpia Milano - Cantù | 55-60 -->
+
+
 <!DOCTYPE html>
 <html lang="it" dir="ltr">
   <head>
@@ -5,6 +14,6 @@
     <title></title>
   </head>
   <body>
-    
+
   </body>
 </html>
