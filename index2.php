8cf5a556db5067fc92295dffc2b12b8e6c0c4181
amedeopro <amedeopro@me.com>
2019-01-22 23:07:24 +0100

    inizio secondo esercizio

--- index2.php
+++ index2.php
@@ -5,15 +5,33 @@ punti fatti dalla squadra in trasferta. Stampiamo a schermo tutte le
 partite con questo schema, tenendo conto che il punteggio potrebbe non
 essere disponibile
 Olimpia Milano - Cantù | 55-60 -->
+<?php
+
+$partite = [
+  [
+    'squadra_casa' => 'Olimpia Milano',
+    'squadra_ospite' => 'Cantu',
+    'punti_casa' => 55,
+    'punti_ospite' => 60
+  ],
+  [
+    'squadra_casa' => 'Team2',
+    'squadra_ospite' => 'Team3',
+    'punti_casa' => 84,
+    'punti_ospite' => 65
+  ],
+  [
+    'squadra_casa' => 'Team4',
+    'squadra_ospite' => 'Team5',
+    'punti_casa' => 101,
+    'punti_ospite' => 103
+  ]
+];
+
+  foreach ($partite as $k => $partita) {
+    var_dump($partita);
+    die();
+  }
 
 
-<!DOCTYPE html>
-<html lang="it" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-
-  </body>
-</html>
+ ?>
